954957ec56490ad3479e1b4c1b9c9175edcfaba6
Mark Samberg <mark.samberg@dpi.nc.gov>
2024-05-30 10:25:55 -0400

    Added template

---
+++ template/extracts.sql
@@ -0,0 +1,20 @@
+/*
+	Title: SQL Report Template
+	
+	Description:
+	Modify the title above to be the title of this report/extract. Modify this description section to be a more detailed description of the file and what it's used for. You can also modify or delete the author section and the revision history.
+	
+	Author: IC User (icuser@schooldistrict.k12.nc.us)
+	
+	Revision History:
+	05/30/2024		Initial creation of this template
+
+*/
+
+
+
+--Comment for SQL Statement 1
+SELECT * FROM students;
+
+--Comment for SQL Statement 2
+SELECT * FROM students;
---
+++ template/readme.md
@@ -0,0 +1,3 @@
+# [VENDOR NAME] Integration Information
+
+Update this readme file with any information about the required steps or information needed to integrate with this vendor.
