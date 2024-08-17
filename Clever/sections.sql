3c253b96f1fabbfb034fd0dd4e35828d762cda89
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-16 23:45:10 -0400

    Create sections.sql

---
+++ Clever/sections.sql
@@ -0,0 +1,36 @@
+/*
+	Title: Clever Sections
+	
+	Description: 
+	
+	Author: Jeremiah Jackson NCDPI
+	
+	Revision History:
+	08/16/2024		Initial creation of this template
+
+*/
+
+
+
+select 
+    sch.number AS 'School_id',
+    s.sectionID AS 'Section_id',
+    t.personid AS 'Teacher_id',
+    s.number AS 'Section_number',
+    c.name AS 'Course_name',
+    p.name AS 'Period',
+    sp.termName AS 'Term_name',
+    sp.termStartDate AS 'Term_start',
+    sp.termEndDate AS 'Term_end'
+
+from v_OneRosterSectionPlacement sp
+INNER JOIN section s ON sp.sectionID = s.sectionID
+INNER JOIN course c ON c.courseID = s.CourseID
+INNER JOIN v_OneRosterCourse orc ON c.courseID = orc.courseID
+INNER JOIN period p ON p.periodID = sp.periodID
+INNER JOIN v_OneRosterTeacher t ON t.personID = s.teacherPersonID
+INNER JOIN calendar cal ON cal.calendarID = c.calendarID
+INNER JOIN School sch ON sch.schoolID = orc.schoolID
+
+WHERE cal.startDate<=GETDATE() AND cal.endDate>=GETDATE() --Get only calendars for the current year
+     
