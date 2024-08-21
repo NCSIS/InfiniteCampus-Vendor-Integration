a4de2ddee63f97fff14cfc172ac37011b83e377b
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-20 22:18:14 -0400

    Create sections.sql

---
+++ Panorama/sections.sql
@@ -0,0 +1,36 @@
+/*
+	Title:Panorama - Sections
+	
+	Description: 
+	This is showing all active students and all enrollment changes. 
+	
+	Author: Jeremiah Jackson - NCDPI
+	
+	Revision History:
+	08/20/2024		Initial creation of this template
+
+*/
+
+
+SELECT
+	courseInfo.courseID, courseinfo.courseNumber, courseinfo.courseName,
+	gradingTaskCredit.credit,
+	sectioninfo.sectionID, sectioninfo.sectionNumber, sectioninfo.teacherDisplay, sectioninfo.teacher2Display, sectioninfo.teacher3Display, sectioninfo.teacher4Display,
+	sectionSchedule.termStart, sectionSchedule.termEnd, sectionSchedule.terms, sectionSchedule.schedules,
+	courseInfo.departmentID, courseInfo.departmentName, sectionSchedule.periodStart, sectionSchedule.periodEnd,
+	sectionInfo.teacherPersonID, sectionInfo.teacher2PersonID, sectionInfo.teacher3PersonID, sectionInfo.teacher4PersonID,
+	cal.calendarID, sch.number AS 'cal.schoolID', cal.name
+	
+FROM v_SectionInfo sectioninfo
+	INNER JOIN v_CourseInfo courseInfo ON sectioninfo.courseID = courseinfo.courseID
+	INNER JOIN calendar cal ON cal.calendarid = sectioninfo.calendarID
+    INNER JOIN v_GradingTaskCredit gradingTaskCredit ON sectioninfo.courseID = gradingTaskCredit.courseID
+	INNER JOIN v_SectionSchedule sectionSchedule ON sectionschedule.sectionid = sectioninfo.sectionID
+	INNER JOIN school sch ON courseinfo.schoolID = sch.schoolID
+WHERE cal.calendarId=sectioninfo.calendarId
+   AND cal.startDate<=GETDATE() AND cal.endDate>=GETDATE() 
+	
+	
+	
+	
+	
