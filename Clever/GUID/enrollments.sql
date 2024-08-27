70b7ebe20e0266ff9eee6eebb9160e8aa9162128
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-27 10:15:42 -0400

    Create enrollments.sql

---
+++ Clever/GUID/enrollments.sql
@@ -0,0 +1,19 @@
+/*
+	Title: Clever Enrollments
+	
+	Author: Jeremiah Jackson NCDPI
+	
+	Revision History:
+	08/16/2024		Initial creation of this template
+
+*/
+
+select
+    sch.identifier AS 'School_id',
+    e.sectionID AS 'Section_id',
+    stu.personGUID AS 'Student_id'
+
+
+from V_OneRosterStudentEnrollment e
+INNER JOIN v_OneRosterSchool sch ON sch.SchoolID = e.schoolID
+INNER JOIN student stu ON stu.personID = e.personid
