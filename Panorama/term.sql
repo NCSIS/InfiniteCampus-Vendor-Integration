b8cc075e34eb2e7bc7c2513a77b5dd3997373b07
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-27 14:55:09 -0400

    Update term.sql

--- Panorama/term.sql
+++ Panorama/term.sql
@@ -23,5 +23,7 @@ FROM Calendar AS cal
 LEFT JOIN ScheduleStructure AS ss ON ss.calendarID = cal.calendarID
 LEFT JOIN TermSchedule AS ts ON ts.structureID = ss.structureID
 INNER JOIN Term AS term ON term.termScheduleID = ts.termScheduleID
+INNER JOIN school sch on sch.schoolid = cal.schoolid
 WHERE cal.calendarId=ss.calendarId
    AND cal.startDate<=GETDATE() AND cal.endDate>=GETDATE() 
+   AND (CAST(substring(sch.number,4,3) AS INTEGER) >= 300 or substring(sch.number,4,3) = '000')
