937065311dfd139dae877e900dbe5da36fd657ce
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-27 15:04:20 -0400

    Update gpa.sql

--- Panorama/gpa.sql
+++ Panorama/gpa.sql
@@ -1,7 +1,7 @@
 /*
 	Title:Panorama - GPA
 	
-	Description: 
+	Description: This pulls GPA As of this school year only
 	
 	Author: Jeremiah Jackson - NCDPI
 	
@@ -10,11 +10,17 @@
 
 */
 
-SELECT
+SELECT DISTINCT
 	student.personID, student.stateID, student.studentNumber, student.lastName, student.firstName, 
 	gpa.cumGpaBasic, gpa.cumGpaUnweighted, 
 	cal.calendarID, sch.number as 'cal.schoolID', cal.name
+
 FROM v_CumGPAFull gpa
 	INNER JOIN v_adhocStudent student ON student.personID = gpa.personID
-	INNER JOIN school sch ON school.schoolID = student.schoolID
+	INNER JOIN school sch ON sch.schoolID = student.schoolID
 	INNER JOIN calendar cal ON cal.calendarID = student.calendarID
+
+WHERE
+    cal.startDate<=GETDATE() AND cal.endDate>=GETDATE() --Get only calendars for the current year
+   AND (student.endDate IS NULL or student.endDate>=GETDATE()) --Get students with no end-date or future-dated end date
+   AND (CAST(substring(sch.number,4,3) AS INTEGER) >= 300 or substring(sch.number,4,3) = '000')
