250683cbed672a6606d8f6887240dddadbe4c977
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-08-29 11:00:43 -0400

    Update extract.sql

--- FinalSiteConnect/extract.sql
+++ FinalSiteConnect/extract.sql
@@ -2,23 +2,17 @@
 	Title: FinalSite Connect (aka: ConnectED, Blackboard Connect)
 	
 	Description:
-	Create CSV for student contacts to be uploaded into FinalSite Connect
+	Create CSV for student contacts to be uploaded into FinalSite Connect.
 	
-	Author: Haywood County Schools - JJ
+	Author: Jeremiah Jackson - NCDPI
 	
 	Revision History:
-	08/06/2024		Initial creation of this template
-	08/22/2024		Fixed a student filter issue and added '000' schools to support charters
-
+	08/06/2024		Initial creation of this template.
+	08/22/2024		Fixed a student filter issue and added '000' schools to support charters.
+        08/28/2024		Streamlined and only selecting guardians
 */
 
-
-
---
-
-
-
--- Go through the contacts table and remove any duplicates.  Usually caused by the same contact being associated with the student more than once.
+-- Remove duplicates from contacts based on key attributes.
 WITH ContactsGrouped AS (
 	SELECT DISTINCT
 		cg.personID, 
@@ -34,14 +28,17 @@ WITH ContactsGrouped AS (
 		cg.state, 
 		cg.zip,
 		cg.seq,
-		cg.relationship
-FROM  v_CensusContactSummary cg WITH (NOLOCK)
-GROUP BY cg.personID,cg.contactPersonID,cg.lastname, cg.firstName, cg.email, cg.homePhone, cg.cellPhone, cg.addressLine1, cg.addressLine2, cg.city, cg.zip, cg.seq, cg.relationship, cg.state
+		cg.relationship,
+		cg.guardian
+	FROM  
+		v_CensusContactSummary cg WITH (NOLOCK)
+	GROUP BY 
+		cg.personID, cg.contactPersonID, cg.lastName, cg.firstName, cg.email, 
+		cg.homePhone, cg.cellPhone, cg.addressLine1, cg.addressLine2, 
+		cg.city, cg.state, cg.zip, cg.seq, cg.relationship, cg.guardian
 ),
 
-
--- Go through the Contacts Table and find the two contacts with the highest emergency priority (usually #1 and #2 but if no #1 it starts at the next number.)  and put their data into c1, c2
--- See Below to only pull Contacts marked as guardians.  This is the preferred method but requires every student to have a guardian.
+-- Get contacts with the highest emergency priority, excluding 'Self' relationships.
 ContactsOrdered AS (
 	SELECT DISTINCT
 		co.personID, 
@@ -59,17 +56,15 @@ ContactsOrdered AS (
 		co.seq,
 		co.relationship,
 		ROW_NUMBER() OVER (PARTITION BY co.personID ORDER BY co.seq) AS rowNumber
-    FROM 
-		contactsGrouped co WITH (NOLOCK)
-    WHERE 
-                co.relationship <> 'Self'
-                AND co.seq IS NOT NULL
-
--- Uncomment the line below to only pull guardians. 
---		AND c.guardian = 1
+	FROM 
+		ContactsGrouped co WITH (NOLOCK)
+	WHERE 
+		co.relationship <> 'Self'
+		AND co.seq IS NOT NULL
+		AND co.guardian = 1  -- Uncomment this line to pull only guardians.
 ),
 
--- Pull the contact for the student and not anyone else associated.
+-- Get contact information for the student themselves.
 ContactSelf AS (
 	SELECT DISTINCT
 		c.personID, 
@@ -80,54 +75,43 @@ ContactSelf AS (
 		c.seq,
 		c.relationship,
 		ROW_NUMBER() OVER (PARTITION BY c.personID ORDER BY c.seq) AS rowNumber
-    FROM 
+	FROM 
 		v_CensusContactSummary c WITH (NOLOCK)
-    WHERE 
-                c.relationship = 'Self'
+	WHERE 
+		c.relationship = 'Self'
 )
 
-
-
-
--- The script starts here.  The above it to remove duplicates and filter the contacts table.
-
-
-select 
+-- Final output query
+SELECT 
     sch.number AS 'INSTITUTION',
 	'student' AS 'CONTACTTYPE',
 	stu.stateID AS 'REFERENCECODE',
 	stu.firstname AS 'FIRSTNAME',
 	stu.lastname AS 'LASTNAME',
 	stu.grade AS 'GRADE',
-	
--- Use homeprimarylanguage.  If blank set to english.  Finaliste only supports english and spanish
 	CASE
 	   WHEN stu.homeprimarylanguage = 'eng' THEN 'English'
 	   WHEN stu.homeprimarylanguage = 'spa' THEN 'Spanish'
 	   ELSE 'English' 
 	END AS 'LANGUAGE',
-
-	'CLIENT' AS 'DATAPROVIDER',
-	-- The first non blank field will be chosen in this order for primary phone
-	COALESCE(cs.householdphone,c1.homePhone, c1.cellphone, c2.homephone, c2.cellphone) AS 'PRIMARYPHONE',
-	c1.cellphone AS 'MOBILEPHONE',
-	c1.homephone AS 'HOMEPHONE',
-	c2.cellphone AS 'MOBILEPHONEALT',
-	c2.homephone AS 'HOMEPHONEALT',
---	COALESCE(cs.householdphone,c1.homePhone, c1.cellphone, c2.homephone, c2.cellphone) AS 'ATTENDANCEPHONE',
-    c1.email AS 'EMAILADDRESS',
+	COALESCE(cs.householdphone, c1.homePhone, c1.cellPhone, c2.homePhone, c2.cellPhone) AS 'PRIMARYPHONE',
+	c1.cellPhone AS 'MOBILEPHONE',
+	c1.homePhone AS 'HOMEPHONE',
+	c2.cellPhone AS 'MOBILEPHONEALT',
+	c2.homePhone AS 'HOMEPHONEALT',
+	c1.email AS 'EMAILADDRESS',
 	c2.email AS 'EMAILADDRESSALT'
-
-
-from v_AdHocStudent stu
+FROM 
+	v_AdHocStudent stu
 LEFT OUTER JOIN ContactsOrdered c1 ON stu.personID = c1.personID AND c1.rowNumber = 1
 LEFT OUTER JOIN ContactsOrdered c2 ON stu.personID = c2.personID AND c2.rowNumber = 2
 LEFT OUTER JOIN ContactSelf cs ON stu.personID = cs.personID AND cs.rowNumber = 1
 LEFT OUTER JOIN school sch ON sch.schoolID = stu.schoolID
+INNER JOIN calendar cal ON cal.calendarID = stu.calendarID
 WHERE 
--- Select the current school year
-	stu.activeYear = 1
-	AND (stu.studentNumber IS NOT NULL or stu.studentNumber <> '')
-	AND (CAST(substring(sch.number,4,3) AS INTEGER) >= 300 or substring(sch.number,4,3) = '000')
-	AND (ISNUMERIC(stu.grade) = '1' OR stu.grade = 'KG' OR stu.grade = 'OS')
-    AND stu.activeToday = 1
+    cal.startDate <= GETDATE() 
+    AND cal.endDate >= GETDATE()  -- Get only calendars for the current year.
+    AND (stu.endDate IS NULL OR stu.endDate >= GETDATE())  -- Get students with no end-date or future-dated end date.
+    AND (CAST(SUBSTRING(sch.number, 4, 3) AS INTEGER) >= 300 OR SUBSTRING(sch.number, 4, 3) = '000')
+    AND (ISNUMERIC(stu.grade) = 1 OR stu.grade = 'KG' OR stu.grade = 'OS')
+    AND stu.stateID IS NOT NULL;
