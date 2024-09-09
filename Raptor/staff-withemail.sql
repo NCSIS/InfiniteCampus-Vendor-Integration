3f76ee73cf10d3c2df907b2ee29015525f346ee6
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2024-09-09 12:10:08 -0400

    Create staff-withemail.sql

---
+++ Raptor/staff-withemail.sql
@@ -0,0 +1,31 @@
+
+/*
+	Title: Raptor Staff with email
+	
+	Description: Change the email address at the bottom to your schools domain.
+                     
+	
+	Author: Jeremiah Jackson NCDPI
+	
+	Revision History:
+	09/09/2024		Initial creation of this template
+
+*/
+
+
+SELECT DISTINCT
+     sm.firstName as 'First_name',
+	 sm.lastname as 'Last_name',
+	 sm.middleName as 'Middle Name',
+	 sm.staffstateID as 'ID Number',
+     sm.title as 'Type',
+	 sm.schoolnumber as 'School ID',
+	 sm.schoolname AS 'School Name',
+	 c.email as 'Staff Email'
+
+
+FROM staffmember sm 
+   INNER JOIN contact c ON sm.personid = c.personid AND c.email IS NOT NULL
+WHERE (sm.endDate IS NULL OR sm.enddate > getdate())
+	AND c.email LIKE 'cravenk12.org'
+
