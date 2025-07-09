ffd86d960fd1c387b533d7df88a3a759af7c357c
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2025-07-09 09:17:21 -0400

    Update extracts.sql

--- One to One Plus/extracts.sql
+++ One to One Plus/extracts.sql
@@ -1,10 +1,11 @@
 /*
 	Title: One to One Plus
 	
-	Author: Watauga County Schools
+	Author: Jeremiah Jackson - NCDPI
 	
 	Revision History:
 	07/25/2024		Initial creation of this template
+        07/09/2025		Updated to allow enrolled students before their start date
 
 */
 
@@ -52,18 +53,23 @@ c2.firstName AS 'guardian2_firstName',
 c2.email AS 'guardian2_email',
 c2.homePhone AS 'guardian2_homePhone',
 c2.cellPhone AS 'guardian2_cellPhone',
-s.activeToday,
+	CASE 
+	    WHEN s.endDate IS NULL OR s.endDate >= GETDATE() THEN 'Active'
+		WHEN s.endDate < GETDATE() THEN 'Inactive'
+		ELSE 'I'
+	END AS 'Status',
 sch.number AS 'School_number'
-FROM
-v_AdHocStudent s
-LEFT OUTER JOIN
-ContactsOrdered c1 ON s.personID = c1.personID AND c1.rowNumber = 1
-LEFT OUTER JOIN
-ContactsOrdered c2 ON s.personID = c2.personID AND c2.rowNumber = 2
-JOIN
-school sch ON sch.schoolid = s.schoolID
-JOIN
-contact c ON c.personID = s.personID
-WHERE
-s.activeYear = 1
-AND s.activeToday = 1
+
+FROM v_AdHocStudent s
+LEFT OUTER JOIN ContactsOrdered c1 ON s.personID = c1.personID AND c1.rowNumber = 1
+LEFT OUTER JOIN ContactsOrdered c2 ON s.personID = c2.personID AND c2.rowNumber = 2
+JOIN school sch ON sch.schoolid = s.schoolID
+JOIN contact c ON c.personID = s.personID
+JOIN calendar cal ON cal.calendarID = s.calendarId
+
+WHERE s.calendarId = cal.calendarid
+    AND cal.startDate <= GETDATE() AND cal.endDate >= GETDATE()
+    AND (CAST(SUBSTRING(sch.number, 4, 3) AS INTEGER) >= 300 OR SUBSTRING(sch.number, 4, 3) = '000')
+    AND s.servicetype = 'P'
+    AND (s.stateid IS NOT NULL OR s.stateid <> '')
+    AND (s.endDate IS NULL or s.endDate>=GETDATE()) --Get students with no end-date or future-dated end date
