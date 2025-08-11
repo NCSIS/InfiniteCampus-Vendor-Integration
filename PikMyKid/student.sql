8575858d5232625dccf62e0f52f1b2f8ad9961d3
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2025-08-10 20:58:14 -0400

    Update student.sql

--- PikMyKid/student.sql
+++ PikMyKid/student.sql
@@ -8,57 +8,60 @@
 	
 	Revision History:
         08/10/2025 - Original
+		08/10/2025 r2 - Fixed Duplicates and moved guardian2 into the correct spots
 
 */
-
-WITH ContactCombined AS (
+WITH ContactsOrdered AS (
     SELECT
-        c.personid, c.email, c.homephone, c.workphone, c.cellphone, c.pager, c.communicationLanguage, 
-        a.city, a.state, a.zip, a.postOfficeBox, a.number, a.street, a.tag, a.apt, 
-        hh.phone, 
-        i.homePrimaryLanguage, i.gender, i.lastName, i.firstName, i.middleName, i.birthdate,
-        ROW_NUMBER() OVER (PARTITION BY c.personid ORDER BY c.personid) AS rowNumber
-    FROM person p 
-        INNER JOIN contact c ON p.personid = c.personid
-        INNER JOIN [Identity] i ON p.currentIdentityID = i.identityID 
-        LEFT OUTER JOIN householdmember hm ON hm.personid = p.personid AND (hm.enddate IS NULL OR hm.enddate >= GETDATE()) AND hm.secondary = '0'
-        LEFT OUTER JOIN household hh ON hh.householdid = hm.householdid
-        LEFT OUTER JOIN householdlocation hl ON hl.householdid = hh.householdid AND (hl.enddate IS NULL OR hl.enddate >= GETDATE()) AND hl.secondary = '0'
-        LEFT OUTER JOIN address a ON hl.addressid = a.addressid
+        cg.personID,
+        cg.contactPersonID,
+        cg.guardian,
+        cg.firstName,
+        cg.lastName,
+        cg.cellPhone,
+        cg.homePhone,
+        cg.householdPhone,
+        cg.email,
+        cg.relationship,
+        cg.relatedby,
+        ROW_NUMBER() OVER (
+            PARTITION BY cg.personID
+            ORDER BY 
+                /* prioritize actual guardians, then your preferred sort */
+                CASE WHEN cg.guardian = '1' THEN 0 ELSE 1 END,
+                cg.relatedby,            -- or cg.relationship
+                cg.contactPersonID DESC  -- deterministic tie-break
+        ) AS rn
+    FROM v_CensusContactSummary cg WITH (NOLOCK)
+    INNER JOIN student stu ON cg.personID = stu.personID
+    WHERE
+        (stu.endDate IS NULL OR stu.endDate >= GETDATE())
+        AND cg.guardian = '1'
 )
-
-
-SELECT DISTINCT
-	stu.firstName AS 'Student FirstName',
-	stu.lastName AS 'Student LastName',
-    sch.number AS 'PMK/School_ID',
-	stu.grade AS 'Grade',
-	'Ignore' AS 'MostUsedPickupMode',
-	transbuspm.number AS 'BusRoute',
-	cg1.firstName AS 'Guardian1FirstName',
-	cg1.lastName AS 'Guardian1LastName',
-	cg1.cellPhone AS 'Guardian1Mobile',
-	cg2.firstName AS 'Guardian2FirstName',
-	cg2.lastName AS 'Guardian2LastName',
-	cg2.cellPhone AS 'Guardian2Mobile',
-	stu.homeroomTeacher AS 'Homeroom/Classroom Name',
-	stu.stateid AS 'ExternalID/Student Number'
-	
-FROM
-    v_adhocstudent stu
-    JOIN Calendar cal ON cal.calendarID = stu.calendarID
-    JOIN School sch ON sch.schoolID = cal.schoolID
-    LEFT OUTER JOIN ContactCombined cg1 ON cg1.personid = stu.personID AND cg1.rowNumber = '1'
-    LEFT OUTER JOIN ContactCombined cg2 ON cg2.personid = stu.personID AND cg2.rowNumber = '2'
-    LEFT JOIN TransportationRoute TransPM ON TransPM.personid = stu.personID AND TransPM.routeTypeCode = 'PM'
-    LEFT JOIN TransportationBus TransBusPM ON TransPM.busid = TransbusPM.busid
-
-WHERE cal.calendarId=stu.calendarId
-   AND sch.schoolID=cal.SchoolID
-   AND cal.startDate<=GETDATE() AND cal.endDate>=GETDATE() --Get only calendars for the current year
-   AND (stu.endDate IS NULL or stu.endDate>=GETDATE()) --Get students with no end-date or future-dated end date
-   AND (CAST(substring(sch.number,4,3) AS INTEGER) >= 300 or substring(sch.number,4,3) = '000')
-   AND stu.stateid IS NOT NULL
-
-
-
+SELECT
+    stu.firstName AS [Student FirstName],
+    stu.lastName  AS [Student LastName],
+    sch.number    AS [PMK/School_ID],
+    stu.grade     AS [Grade],
+    'Ignore'      AS [MostUsedPickupMode],
+    transbuspm.number AS [BusRoute],
+    cg1.firstName AS [Guardian1FirstName],
+    cg1.lastName  AS [Guardian1LastName],
+    cg1.cellPhone AS [Guardian1Mobile],
+    cg2.firstName AS [Guardian2FirstName],
+    cg2.lastName  AS [Guardian2LastName],
+    cg2.cellPhone AS [Guardian2Mobile],
+    stu.homeroomTeacher AS [Homeroom/Classroom Name],
+    stu.stateid   AS [ExternalID/Student Number]
+FROM v_adhocstudent stu
+JOIN Calendar cal ON cal.calendarID = stu.calendarID
+JOIN School   sch ON sch.schoolID   = cal.schoolID
+LEFT JOIN ContactsOrdered cg1 ON cg1.personID = stu.personID AND cg1.rn = 1
+LEFT JOIN ContactsOrdered cg2 ON cg2.personID = stu.personID AND cg2.rn = 2
+LEFT JOIN TransportationRoute TransPM   ON TransPM.personID = stu.personID AND TransPM.routeTypeCode = 'PM'
+LEFT JOIN TransportationBus  TransBusPM ON TransPM.busID    = TransBusPM.busID
+WHERE 
+    cal.startDate <= GETDATE() AND cal.endDate >= GETDATE()
+    AND (stu.endDate IS NULL OR stu.endDate >= GETDATE())
+    AND (CAST(SUBSTRING(sch.number,4,3) AS INT) >= 300 OR SUBSTRING(sch.number,4,3) = '000')
+    AND stu.stateid IS NOT NULL;
