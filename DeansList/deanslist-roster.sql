8ec348d11e1eed45a69099b2d62803767bc02954
Jeremiah Jackson <jeremiah.jackson@dpi.nc.gov>
2025-08-17 23:31:24 -0400

    Update deanslist-roster.sql

--- DeansList/deanslist-roster.sql
+++ DeansList/deanslist-roster.sql
@@ -19,7 +19,7 @@ select
 	r.sectionid AS 'SectionID',
 	orc.title AS 'SectionName',
 	p.name AS 'Period',
-	s.termName AS 'TERM'
+	sp.termName AS 'TERM'
 	
 FROM roster r
 	INNER JOIN section s on s.sectionID = r.sectionID
